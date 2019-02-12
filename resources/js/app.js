03ac5f9c2f8564c782ae6921f1b4ac2a6dc320b7
aindriu80 <aindriu80@gmail.com>
2019-02-12 20:53:00 +0000

    Populating items from the data structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,56 +1,75 @@
 // Storaage Controller
 
 // Item Controller
-const ItemCtrl = (function(){
-    // Item Constructor
-    const Item = function(id, name, calories){
-        this.id = id;
-        this.name = name;
-        this.calories = calories;
-    }
+const ItemCtrl = (function() {
+  // Item Constructor
+  const Item = function(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
 
-    // Data structure / State
-    const data = {
-        items: [
-            {id:0,name: 'Steak Dinner', calories: 1200},
-            {id:1,name: 'Cookie', calories: 400},
-            {id:2,name: 'Eggs', calories: 300}
+  // Data structure / State
+  const data = {
+    items: [
+      { id: 0, name: "Steak Dinner", calories: 1200 },
+      { id: 1, name: "Cookie", calories: 400 },
+      { id: 2, name: "Eggs", calories: 300 }
     ],
-        currentItem: null,
-        totalCalories: 0
-    }
+    currentItem: null,
+    totalCalories: 0
+  };
 
-    // Public Methods
-    return {
-        logData: function(){
-            return data;
-        }
+  // Public Methods
+  return {
+    getItems: function() {
+      return data.items;
+    },
+    logData: function() {
+      return data;
     }
+  };
 })();
 
 // UI Controller
-const UICtrl = (function(){
-    
-    // Public Methods
-    return {
+const UICtrl = (function() {
+  const UISelectors = {
+    itemList: "#item-list"
+  };
+  // Public Methods
+  return {
+    populateItemList: function(items) {
+      let html = "";
 
+      items.forEach(function(item) {
+        html += `<li class="collection-item" id="item-${item.id}">            
+            <strong>${item.name}</strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+            <i class="edit-item fa fa-pencil"></i></a>
+          </li>
+            `;
+      });
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
     }
-    
+  };
 })();
 
 // App Controller
-    const AppCtrl = (function(Item,UICtrl){
-        // console.log('this',ItemCtrl.logData());
-   
-        // Public methods
-    return {
-        init: function(){
-            console.log('Initializing App...');
-        }
+const AppCtrl = (function(Item, UICtrl) {
+  // console.log('this',ItemCtrl.logData());
+
+  // Public methods
+  return {
+    init: function() {
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
-
-
-})(ItemCtrl,UICtrl);
+  };
+})(ItemCtrl, UICtrl);
 
 // Initialize App
 AppCtrl.init();
